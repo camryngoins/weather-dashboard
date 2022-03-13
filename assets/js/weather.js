3d9902592dde84fb0dba4eaa319318f6662a8e30
camryngoins <camryngoins00@gmail.com>
2022-03-13 12:29:18 -0500

    getting started on js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,13 +1,18 @@
-var button = document.querySelector(".button");
-var inputValue = document.querySelector(".inputValue");
-var name = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
 
-// var apiKey = "1c402c82171a1708d60d6ea0950d1ece"
-// var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey
+var formSubmitHandler = function(event) {
+event.preventDefault();
 
-var getWeather = function() {
-fetch("api.openweathermap.org/data/2.5/weather?q=")
-}
-getWeather();
+};
+// get current weather function
+var getCurrentWeather = function(city) {
+var apiKey = "1c402c82171a1708d60d6ea0950d1ece"
+var queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey 
+
+fetch(queryUrl)
+.then(function(response) {
+    response.json().then(function(data) {
+        displayCurrentWeather(data, city);
+    })
+})
+};
+getCurrentWeather();
