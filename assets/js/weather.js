a126545ec1b95600de9dcd9a25eee1a5791cc04b
camryngoins <camryngoins00@gmail.com>
2022-03-13 11:55:08 -0500

    added bootstrap features to html

---
+++ assets/js/weather.js
@@ -0,0 +1,13 @@
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+
+// var apiKey = "1c402c82171a1708d60d6ea0950d1ece"
+// var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey
+
+var getWeather = function() {
+fetch("api.openweathermap.org/data/2.5/weather?q=")
+}
+getWeather();
