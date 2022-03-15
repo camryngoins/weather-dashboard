d79e0fe07d94cea11a7ab17406def0e4046e8431
camryngoins <camryngoins00@gmail.com>
2022-03-15 12:34:17 -0500

    got current weather

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,18 +1,48 @@
+var searchButton = document.querySelector("#searchButton");
+var searchCity = document.querySelector("#searchCity");
+var cityName = document.querySelector("#cityName");
+var temperature = document.querySelector("#temperature");
+var humidity = document.querySelector("#humidity");
+var windSpeed = document.querySelector("#windSpeed");
+var uvIndex = document.querySelector("#uvIndex");
 
-var formSubmitHandler = function(event) {
-event.preventDefault();
-
-};
-// get current weather function
-var getCurrentWeather = function(city) {
 var apiKey = "1c402c82171a1708d60d6ea0950d1ece"
-var queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey 
 
-fetch(queryUrl)
-.then(function(response) {
+searchButton.addEventListener("click", function() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&appid=" + apiKey)
+    .then(function(response) {
     response.json().then(function(data) {
-        displayCurrentWeather(data, city);
+      displayWeather(data);
     })
-})
-};
-getCurrentWeather();
+  })
+});
+
+var displayWeather = function(data) {
+  console.log(data)
+    var name = data.name
+    var temp = data.main.temp
+    var humid = data.main.humidity
+    var wind = data.wind.speed
+
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey)
+    .then(function(response) {
+      response.json().then(function(data) {
+        var uvi = data.current.uvi
+        uvIndex.innerHTML = uvi;
+      })
+    })
+
+
+
+    cityName.innerHTML= name;
+    temperature.innerHTML = temp;
+    humidity.innerHTML = humid;
+    windSpeed.innerHTML = wind;
+    };
+
+
+
+
+
+
+
